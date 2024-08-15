ef9f858dbb48d6663eec120a2e3d182e859595bb
xiao <xiao@xiaodeMacBook-Pro.local>
2024-08-16 01:46:59 +0800

    update

--- init_2.sh
+++ init_2.sh
@@ -1,119 +1,99 @@
-#关闭selinux
-selinuxset() 
-{
-	selinux_status=`grep "SELINUX=disabled" /etc/sysconfig/selinux | wc -l`
-	echo "========================禁用SELINUX========================"
-	if [ $selinux_status -eq 0 ];then
-		sed  -i "s#SELINUX=enforcing#SELINUX=disabled#g" /etc/sysconfig/selinux
-		setenforce 0
-		echo '#grep SELINUX=disabled /etc/sysconfig/selinux'
-		grep SELINUX=disabled /etc/sysconfig/selinux
-		echo '#getenforce'
-		getenforce
-	else
-		echo 'SELINUX已处于关闭状态'
-		echo '#grep SELINUX=disabled /etc/sysconfig/selinux'
-                grep SELINUX=disabled /etc/sysconfig/selinux
-                echo '#getenforce'
-                getenforce
-	fi
-		action "完成禁用SELINUX" /bin/true
-	echo "==========================================================="
-	sleep 2
-}
-#关闭firewalld
-firewalldset()
-{
-	echo "=======================禁用firewalld========================"
-	systemctl stop firewalld.service &> /dev/null
-	echo '#firewall-cmd  --state'
-	systemctl disable firewalld.service &> /dev/null
-	echo '#systemctl list-unit-files | grep firewalld'
-	systemctl list-unit-files | grep firewalld
-	action "完成禁用firewalld，生产环境下建议启用！" /bin/true
-	echo "==========================================================="
-	sleep 5
-}
-#修改文件描述符
-limitset()
-{
-	echo "======================修改文件描述符======================="
-	echo '* - nofile 65535'>/etc/security/limits.conf
-	echo '* - nproc 65536'>>/etc/security/limits.conf
-	echo '*    soft nofile 1048576'>>/etc/security/limits.conf
-	echo '*    hard nofile 1048576'>>/etc/security/limits.conf
-	ulimit -SHn 65535
-	echo "#cat /etc/security/limits.conf"
-	cat /etc/security/limits.conf
-	echo "#ulimit -Sn ; ulimit -Hn"
-	ulimit -Sn ; ulimit -Hn
-	action "完成修改文件描述符" /bin/true
-	echo "==========================================================="
-	sleep 2
-}
-#安装常用工具及修改yum源
-yumset() {
-    echo "=================安装常用工具及修改yum源==================="
-    yum install wget -y &> /dev/null
-    if [ $? -eq 0 ]; then
-        #cd /etc/yum.repos.d/
-        #\cp CentOS-Base.repo CentOS-Base.repo.$(date +%F)
-        ping -c 1 mirrors.aliyun.com &> /dev/null
-        if [ $? -eq 0 ]; then
-            # 根据操作系统决定如何配置EPEL源
-            #!/bin/bash
-	    # 获取操作系统信息
-	    OS_INFO=$(cat /etc/os-release)
+#!/bin/bash
 
-	    # 检测是否为CentOS 7
-	    if echo "$OS_INFO" | grep -q -i "centos" && echo "$OS_INFO" | grep -q "VERSION_ID=\"7\""; then
-    	    # 如果系统是CentOS 7
-            	echo "检测到CentOS 7系统，配置CentOS的EPEL源"
-    		wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo &> /dev/null
-    		yum install epel-release -y &> /dev/null
-	    else
-    		# 如果系统不是CentOS 7
-    		echo "未检测到CentOS 7系统，假定为Amazon Linux系统，配置Amazon Linux的EPEL源"
-    		amazon-linux-extras install epel -y &> /dev/null
-	    fi
+# 禁用SELINUX
+disable_selinux() {
+    echo "========================禁用SELINUX========================"
+    if grep -q "SELINUX=disabled" /etc/sysconfig/selinux; then
+        echo 'SELINUX已处于关闭状态'
+    else
+        sed -i 's#SELINUX=enforcing#SELINUX=disabled#g' /etc/sysconfig/selinux
+        setenforce 0
+        echo 'SELINUX已禁用'
+    fi
+    grep SELINUX=disabled /etc/sysconfig/selinux
+    getenforce
+    echo "==========================================================="
+    sleep 2
+}
+
+# 禁用firewalld
+disable_firewalld() {
+    echo "=======================禁用firewalld========================"
+    systemctl stop firewalld.service &> /dev/null
+    systemctl disable firewalld.service &> /dev/null
+    echo "firewalld状态：$(systemctl is-active firewalld)"
+    echo "firewalld已禁用"
+    echo "==========================================================="
+    sleep 2
+}
+
+# 修改文件描述符限制
+set_limits() {
+    echo "======================修改文件描述符======================="
+    cat <<EOF > /etc/security/limits.conf
+*    soft nofile 1048576
+*    hard nofile 1048576
+*    soft nproc 65536
+*    hard nproc 65536
+EOF
+    ulimit -SHn 1048576
+    echo "文件描述符限制已修改"
+    ulimit -Sn ; ulimit -Hn
+    echo "==========================================================="
+    sleep 2
+}
+
+# 安装常用工具及修改YUM源
+setup_yum() {
+    echo "=================安装常用工具及修改YUM源==================="
+    yum install -y wget &> /dev/null
+    if [ $? -eq 0 ]; then
+        if ping -c 1 mirrors.aliyun.com &> /dev/null; then
+            OS_INFO=$(cat /etc/os-release)
+            if echo "$OS_INFO" | grep -q "CentOS Linux 7"; then
+                echo "检测到CentOS 7系统，配置CentOS的EPEL源"
+                wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo &> /dev/null
+                yum install -y epel-release &> /dev/null
+            else
+                echo "检测到非CentOS 7系统，配置Amazon Linux的EPEL源"
+                amazon-linux-extras install -y epel &> /dev/null
+            fi
             yum clean all &> /dev/null
             yum makecache &> /dev/null
         else
             echo "无法连接到网络"
-            exit $?
+            exit 1
         fi
     else
         echo "wget安装失败"
-        exit $?
+        exit 1
     fi
-    yum -y install ntpdate lsof net-tools telnet vim lrzsz tree nmap nc sysstat bind-utils htop &> /dev/null
-    echo "完成安装常用工具及修改yum源"
+    yum install -y ntpdate lsof net-tools vim htop &> /dev/null
+    echo "已完成常用工具的安装"
     echo "==========================================================="
     sleep 2
 }
-#设置时间同步
-ntpdateset()
-{
-	echo "=======================设置时间同步========================"
-	yum -y install ntpdate &> /dev/null
-	if [ $? -eq 0 ];then
-		/usr/sbin/ntpdate time.windows.com
-		echo "*/5 * * * * /usr/sbin/ntpdate ntp.aliyun.com &>/dev/null" >> /var/spool/cron/root
-	else
-		echo "ntpdate安装失败"
-		exit $?
-	fi
-	action "完成设置时间同步" /bin/true
-	echo "==========================================================="
-	sleep 2
+
+# 设置时间同步
+set_ntp() {
+    echo "=======================设置时间同步========================"
+    yum install -y ntpdate &> /dev/null
+    if [ $? -eq 0 ]; then
+        ntpdate time.windows.com
+        echo "*/5 * * * * /usr/sbin/ntpdate ntp.aliyun.com &>/dev/null" >> /var/spool/cron/root
+        echo "时间同步已设置"
+    else
+        echo "ntpdate安装失败"
+        exit 1
+    fi
+    echo "==========================================================="
+    sleep 2
 }
-#优化系统内核
-kernelset()
-{
-	echo "======================优化系统内核========================="
-	chk_nf=`cat /etc/sysctl.conf | grep conntrack |wc -l`
-	if [ $chk_nf -eq 0 ];then
-		cat >>/etc/sysctl.conf<<EOF
+
+# 优化系统内核参数
+optimize_kernel() {
+    echo "======================优化系统内核========================="
+    cat <<EOF >> /etc/sysctl.conf
 net.ipv6.conf.all.disable_ipv6 = 1
 net.ipv6.conf.default.disable_ipv6 = 1
 net.ipv4.icmp_echo_ignore_broadcasts = 1
@@ -148,9 +128,6 @@ net.ipv4.tcp_synack_retries = 1
 net.ipv4.tcp_syn_retries = 1
 net.ipv4.tcp_tw_reuse = 1
 net.ipv4.ip_local_port_range = 1024 65535
-net.ipv4.tcp_mem = 6177504 8236672 16777216
-net.ipv4.tcp_rmem = 4096 873800 16777216
-net.ipv4.tcp_wmem = 4096 873800 16777216
 net.ipv4.tcp_fin_timeout = 15
 net.ipv4.tcp_keepalive_time = 30
 fs.file-max = 2097152
@@ -160,25 +137,21 @@ net.ipv4.conf.all.secure_redirects = 0
 net.ipv4.conf.default.secure_redirects = 0
 vm.swappiness = 10
 EOF
-	sysctl -p
-	else
-		echo "优化项已存在。"
-	fi
-	action "内核调优完成" /bin/true
-	echo "==========================================================="
-	sleep 2
+    sysctl -p
+    echo "内核优化完成"
+    echo "==========================================================="
+    sleep 2
 }
-#history优化
-historyset()
-{
-	echo "========================history优化========================"
-	chk_his=`cat /etc/profile | grep HISTTIMEFORMAT |wc -l`
-	if [ $chk_his -eq 0 ];then
-		cat >> /etc/profile <<'EOF'
-#设置history格式
-export HISTTIMEFORMAT="[%Y-%m-%d %H:%M:%S] [`whoami`] [`who am i|awk '{print $NF}'|sed -r 's#[()]##g'`]: "
-#记录shell执行的每一条命令
-export PROMPT_COMMAND='\
+
+# 优化命令历史记录
+optimize_history() {
+    echo "========================history优化========================"
+    if ! grep -q "HISTTIMEFORMAT" /etc/profile; then
+        cat <<'EOF' >> /etc/profile
+# 设置history格式
+export HISTTIMEFORMAT="[%Y-%m-%d %H:%M:%S] [`whoami`] [`who am i | awk '{print $NF}' | sed -r 's#[()]##g'`]: "
+# 记录shell执行的每一条命令
+export PROMPT_COMMAND='
 if [ -z "$OLD_PWD" ];then
     export OLD_PWD=$PWD;
 fi;
@@ -188,18 +161,22 @@ fi;
 export LAST_CMD="$(history 1)";
 export OLD_PWD=$PWD;'
 EOF
-		source /etc/profile
-	else
-		echo "优化项已存在。"
-	fi
-	action "完成history优化" /bin/true
-	echo "==========================================================="
-	sleep 2
+        source /etc/profile
+        echo "history优化已完成"
+    else
+        echo "history已优化"
+    fi
+    echo "==========================================================="
+    sleep 2
 }
-selinuxset
-firewalldset
-limitset
-yumset
-ntpdateset
-kernelset
-historyset
+
+# 执行所有优化配置
+disable_selinux
+disable_firewalld
+set_limits
+setup_yum
+set_ntp
+optimize_kernel
+optimize_history
+
+echo "系统优化已完成！"
